5b507084bdbc53d4585ffeb329a2b2436fd55fc3
projecttzzeus <91053488+projecttzzeus@users.noreply.github.com>
2021-09-20 23:45:28 +0530

    Update choices.js

--- js/extention/choices.js
+++ js/extention/choices.js
@@ -1,17 +1,1 @@
-function search_animal() {
-    var parser, xmlDoc;
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(text,"indelfile.xml");
-    let input = document.getElementById('searchbar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('animals');
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!xmlDoc[i].innerHTML.toLowerCase().includes(input)) {
-            xmlDoc[i].style.display="none";
-        }
-        else {
-            xmlDoc[i].style.display="list-item";                 
-        }
-    }
-}
+
